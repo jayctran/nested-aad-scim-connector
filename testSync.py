af0d35e68a2a36af0a4773a478b91c7a6315cdf7
James (PC) <james.tran@rivirhouse.com>
2024-04-18 11:35:50 +1000

    Removed unnecessary files with working env

--- testSync.py
+++ testSync.py
@@ -1,4 +1,9 @@
 from nestedaaddb.nested_groups import SyncNestedGroups
-sn = SyncNestedGroups()
-sn.loadConfig("/Users/abhishekpratap.singh/Desktop/nestedAADSynBakUp16Nov/config/config.cfg")
+import asyncio
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+sn = SyncNestedGroups(loop)
 sn.sync("Parent",True)
+loop.close()
